82cdf1312d9a852dc0122cd8ffbb2a596b822339
awesomecat <the.awesome.e.cat@gmail.com>
2026-01-11 16:22:15 -0600

    added forgoten semicolon

--- scripts/footer.js
+++ scripts/footer.js
@@ -4,4 +4,4 @@ document.querySelector("footer").append(FOOTER);
 const dateObject = new Date()
 const lastModified = document.lastModified
 FOOTER.innerHTML = "<hr> <p>Copyright (c)" + dateObject.getFullYear() + "<br> Last Modified on: " + lastModified + "</p>";
-FOOTER.style.fontSize = "8pt"
+FOOTER.style.fontSize = "8pt";
