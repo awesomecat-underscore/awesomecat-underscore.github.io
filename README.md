cfa560cb51fc40645981e3ce914b1e6a39d176d4
awesomecat-underscore <the.awesome.e.cat@gmail.com>
2026-04-11 22:33:59 -0500

    agagag

--- README.md
+++
@@ -1,2 +0,0 @@
-# the_awesomecat.github.io
-my website, where i do things
--- scripts/footer.js
+++
@@ -1,7 +0,0 @@
-const FOOTER = document.createElement("footer");
-document.querySelector("footer").append(FOOTER);
-
-const dateObject = new Date()
-const lastModified = document.lastModified
-FOOTER.innerHTML = "<hr> <p>Copyright (c)" + dateObject.getFullYear() + "<br> Last Modified on: " + lastModified + "</p>";
-FOOTER.style.fontSize = "8pt";
