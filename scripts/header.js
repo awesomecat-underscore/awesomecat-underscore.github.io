0682f735125555b3cdbdc54f125d831e17e9f9d0
awesomecat-underscore <the.awesome.e.cat@gmail.com>
2026-04-12 21:19:59 -0500

    redo things

---
+++ scripts/header.js
@@ -0,0 +1,11 @@
+var nav = document.getElementById("navbar");
+
+var logo = '<img style="size: 25%;" src="/images/acabode.png" alt="acabode logo">'
+var title = "<h1>Awesomecat's Abode</h1>";
+
+nav.innerHTML = logo + title
+
+nav.style.textAlign = "center";
+
+nav.style.fontFamily = "Arial"
+nav.style.fontSize = "12px"
